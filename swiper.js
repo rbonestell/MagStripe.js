cf83ff988e2374bfae622ddf2b4fafa5c00314df
Bobby Bonestell <rbonestell@gmail.com>
2015-12-17 20:56:42 -0700

    Initial commit

---
+++ swiper.js
@@ -0,0 +1,40 @@
+/**
+ * Capture data from swiper
+ * @param {String} swipeDestinationControlID ID of TEXT or HIDDEN input control to populate with swipe data
+ */
+function getSwipe(swipeDestinationControlID)
+{
+  sdc = document.getElementById(swipeDestinationControlID);
+  if (sdc)
+  {
+    var controlType = sdc.getAttribute('type');
+    if (controlType == "text" || controlType == "hidden")
+    {
+      sdc.value = "";
+      document.addEventListener('keypress', function(event)
+        {
+          event.preventDefault();
+          if(event.which == 13) // On ENTER submit parent form
+          {
+            document.removeEventListener('keypress');
+            sdc.form.submit();
+          }
+          else
+          {
+            sdc.value += String.fromCharCode(event.which);
+          }
+          return false;
+        });
+    }
+  }
+}
+
+/*
+* Cancel swipe capture
+*/
+function cancelSwipe()
+{
+  if (sdc)
+    sdc.value = "";
+  document.removeEventListener('keypress');
+}
